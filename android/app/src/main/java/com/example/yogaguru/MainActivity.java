ae07f17366bc08c645559aa40863afa70dd7a631
waqas8919 <65292506+waqas8919@users.noreply.github.com>
2020-07-23 23:56:40 +0500

    Yoga Guru App code

---
+++ android/app/src/main/java/com/example/yogaguru/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.yogaguru;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
